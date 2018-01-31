6f5e9df2ef62597d0f063b5c14b996f354fbce14
SSE4 <tomskside@gmail.com>
2018-01-31 16:09:15 +0700

    - initial MSVC build prototype

--- conanfile.py
+++ conanfile.py
@@ -1,69 +1,116 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from conans import ConanFile, CMake, tools
+from conans import ConanFile, tools, VisualStudioBuildEnvironment
 import os
 
 
 class LibnameConan(ConanFile):
-    name = "libname"
-    version = "0.0.0"
-    url = "https://github.com/bincrafters/conan-libname"
-    description = "Keep it short"
-
-    # Indicates License type of the packaged library
-    license = "MIT"
-
-    # Packages the license for the conanfile.py
+    name = "czmq"
+    version = "4.1.0"
+    url = "https://github.com/bincrafters/conan-czmq"
+    description = "High-level C binding for ØMQ"
+    license = "MPL-2.0"
     exports = ["LICENSE.md"]
-
-    # Remove following lines if the target lib does not use cmake.
-    exports_sources = ["CMakeLists.txt"]
-    generators = "cmake"
-
-    # Options may need to change depending on the packaged library.
     settings = "os", "arch", "compiler", "build_type"
     options = {"shared": [True, False]}
     default_options = "shared=False"
 
-    # Custom attributes for Bincrafters recipe conventions
-    source_subfolder = "source_subfolder"
-    build_subfolder = "build_subfolder"
-
-    # Use version ranges for dependencies unless there's a reason not to
-    requires = (
-        "OpenSSL/[>=1.0.2l]@conan/stable",
-        "zlib/[>=1.2.11]@conan/stable"
-    )
+    def requirements(self):
+        self.requires.add('zmq/[>=4.2.2]@bincrafters/stable')
 
     def source(self):
-        source_url = "https://github.com/libauthor/libname"
+        source_url = "https://github.com/zeromq/czmq"
         tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.version))
         extracted_dir = self.name + "-" + self.version
 
-        #Rename to "source_subfolder" is a convention to simplify later steps
-        os.rename(extracted_dir, self.source_subfolder)
+        os.rename(extracted_dir, 'sources')
 
+    def build_vs(self):
+        vs_version = int(str(self.settings.compiler.version))
+        toolset = None
+        if vs_version == 9:
+            folder = 'vs2008'
+        elif vs_version == 10:
+            folder = 'vs2010'
+        elif vs_version == 11:
+            folder = 'vs2012'
+        elif vs_version == 12:
+            folder = 'vs2013'
+        elif vs_version == 14:
+            folder = 'vs2015'
+        elif vs_version > 14:
+            folder = 'vs2015'
+            toolset = 'v141'
+        runtime_library = {'MT': 'MultiThreaded',
+                           'MTd': 'MultiThreadedDebug',
+                           'MD': 'MultiThreadedDLL',
+                           'MDd': 'MultiThreadedDebugDLL'}.get(str(self.settings.compiler.runtime))
+
+        libzmq_props = os.path.join('sources', 'builds', 'msvc', 'vs2015', 'libczmq', 'libczmq.props')
+        tools.replace_in_file(libzmq_props, '<ClCompile>',
+                              '<ClCompile><RuntimeLibrary>%s</RuntimeLibrary>' % runtime_library)
+        tools.replace_in_file(libzmq_props, '<Import Project="$(SolutionDir)libzmq.import.props" />', '')
+
+        # generate platform.h
+        with open(os.path.join('sources', 'builds', 'msvc', 'platform.h'), 'w') as f:
+            f.write('#ifndef __PLATFORM_H_INCLUDED__\n')
+            f.write('#define __PLATFORM_H_INCLUDED__\n')
+            f.write('#define CZMQ_HAVE_WINDOWS 1\n')
+            f.write('#define HAVE_LIBZMQ 1\n')
+            f.write('#undef HAVE_UUID\n')
+            f.write('#undef HAVE_SYSTEMD\n')
+            f.write('#undef HAVE_LZ4\n')
+            f.write('#define CZMQ_BUILD_DRAFT_API 1\n')
+            f.write('#endif\n')
+
+        if self.settings.build_type == 'Debug':
+            config = 'DynDebug' if self.options.shared else 'StaticDebug'
+        elif self.settings.build_type == 'Release':
+            config = 'DynRelease' if self.options.shared else 'StaticRelease'
+        with tools.chdir(os.path.join('sources', 'builds', 'msvc', folder)):
+            command = tools.msvc_build_command(self.settings, 'czmq.sln', upgrade_project=False,
+                                               build_type=config, targets=['libczmq'], toolset=toolset)
+            if self.settings.arch == 'x86':
+                command = command.replace('/p:Platform="x86"', '/p:Platform="Win32"')
+            self.run(command)
+
+    def build_configure(self):
+        raise Exception("todo")
 
     def build(self):
-        cmake = CMake(self)
-        cmake.definitions["BUILD_TESTS"] = False # example
-        cmake.configure(build_folder=self.build_subfolder)
-        cmake.build()
-        cmake.install()
+        if self.settings.compiler == 'Visual Studio':
+            env_build = VisualStudioBuildEnvironment(self)
+            with tools.environment_append(env_build.vars):
+                self.build_vs()
+        else:
+            self.build_configure()
 
     def package(self):
-        # If the CMakeLists.txt has a proper install method, the steps below may be redundant
-        # If so, you can replace all the steps below with the word "pass"
-        include_folder = os.path.join(self.source_subfolder, "include")
-        self.copy(pattern="LICENSE", dst="license", src=self.source_subfolder)
-        self.copy(pattern="*", dst="include", src=include_folder)
-        self.copy(pattern="*.dll", dst="bin", keep_path=False)
-        self.copy(pattern="*.lib", dst="lib", keep_path=False)
-        self.copy(pattern="*.a", dst="lib", keep_path=False)
-        self.copy(pattern="*.so*", dst="lib", keep_path=False)
-        self.copy(pattern="*.dylib", dst="lib", keep_path=False)
-
+        with tools.chdir("sources"):
+            self.copy(pattern="LICENSE")
+        if self.settings.compiler == 'Visual Studio':
+            kind = 'dynamic' if self.options.shared else 'static'
+            if self.settings.arch == 'x86':
+                arch = 'Win32'
+            elif self.settings.arch == 'x86_64':
+                arch = 'x64'
+            vs_version = {8: 'v80',
+                          9: 'v90',
+                          10: 'v100',
+                          11: 'v110',
+                          12: 'v120',
+                          14: 'v140',
+                          15: 'v141'}.get(int(str(self.settings.compiler.version)))
+            libdir = os.path.join('sources', 'bin', arch, str(self.settings.build_type), vs_version, kind)
+            self.copy(pattern='*.lib', src=libdir, dst='lib', keep_path=False)
+            self.copy(pattern='*.dll', src=libdir, dst='bin', keep_path=False)
+            self.copy(pattern='*.h', src=os.path.join('sources', 'include'), dst='include', keep_path=True)
 
     def package_info(self):
-        self.cpp_info.libs = tools.collect_libs(self)
+        if self.settings.compiler == 'Visual Studio':
+            self.cpp_info.libs = ['libczmq', 'rpcrt4']
+            if not self.options.shared:
+                self.cpp_info.defines.append('CZMQ_STATIC')
+        else:
+            self.cpp_info.libs = ['czmq']
--- test_package/test_package.cpp
+++ test_package/test_package.cpp
@@ -1,8 +1,15 @@
-#include <cstdlib>
-#include <iostream>
-
-int main()
+#include <czmq.h>
+int main (void)
 {
-    std::cout << "Bincrafters\n";
-    return EXIT_SUCCESS;
+    zsock_t *push = zsock_new_push ("inproc://example");
+    zsock_t *pull = zsock_new_pull ("inproc://example");
+    zstr_send (push, "Hello, World");
+
+    char *string = zstr_recv (pull);
+    puts (string);
+    zstr_free (&string);
+
+    zsock_destroy (&pull);
+    zsock_destroy (&push);
+    return 0;
 }
